0738306b53f800d6ff054f12e3725a93aa27c4ce
sam hockaday <samuel.hockaday@gmail.com>
2024-01-01 16:50:16 +0000

    added function to calculate the time until for days hours mins and seconds

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 // Create variables for each time calculation
-const days = document.querySelectorAll('days');
-const hours = document.querySelectorAll('hours');
-const minutes = document.querySeclectorAll('minutes');
-const seconds = document.querySelectorAll('seconds');
+const days = document.querySelector('#days');
+const hours = document.querySelector('#hours');
+const minutes = document.querySelector('minutes');
+const seconds = document.querySelector('seconds');
 
 // Hard code the target date (New Year 2025)
 const currentYear = new Date().getFullYear();
 
 const newYear25 = new Date (`January 1 ${currentYear + 1} 00:00:00`);
 
+//Add function to update the coundown elements 
+
+function updateCountdown(){
+    const currentTime = new Date();
+    const timeDifference = newYear25 - currentTime;
+
+
+    const d = Math.floor((timeDifference / 1000 * 60 * 60 * 24));
+    const h = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const m = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    const s = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+console.log(d);
+console.log(h);
+console.log(m);
+console.log(s);
+}
+
+updateCountdown();
+console.log("Hello world");
