4da51337759a8a36cfc5d588cd2265f143cd6b5d
sam hockaday <samuel.hockaday@gmail.com>
2024-01-01 16:17:41 +0000

    changed app to be a countdown to 2025.

---
+++ index.js
@@ -0,0 +1,11 @@
+// Create variables for each time calculation
+const days = document.querySelectorAll('days');
+const hours = document.querySelectorAll('hours');
+const minutes = document.querySeclectorAll('minutes');
+const seconds = document.querySelectorAll('seconds');
+
+// Hard code the target date (New Year 2025)
+const currentYear = new Date().getFullYear();
+
+const newYear25 = new Date (`January 1 ${currentYear + 1} 00:00:00`);
+
