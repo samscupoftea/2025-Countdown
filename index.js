2935e739e44edcc0fddb01d063df48d87c3e8484
sam hockaday <samuel.hockaday@gmail.com>
2024-01-01 17:01:51 +0000

    working countdown timer. Fixed issue with seconds being captial S and parthenthesis on year calc

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Create variables for each time calculation
 const days = document.querySelector('#days');
 const hours = document.querySelector('#hours');
-const minutes = document.querySelector('minutes');
-const seconds = document.querySelector('seconds');
+const minutes = document.querySelector('#minutes');
+const seconds = document.querySelector('#seconds');
 
 // Hard code the target date (New Year 2025)
 const currentYear = new Date().getFullYear();
@@ -16,7 +16,7 @@ function updateCountdown(){
     const timeDifference = newYear25 - currentTime;
 
 
-    const d = Math.floor((timeDifference / 1000 * 60 * 60 * 24));
+    const d = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
     const h = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const m = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
     const s = Math.floor((timeDifference % (1000 * 60)) / 1000);
@@ -25,7 +25,13 @@ console.log(d);
 console.log(h);
 console.log(m);
 console.log(s);
+
+days.innerHTML = d;
+hours.innerHTML = h;
+minutes.innerHTML = m;
+seconds.innerHTML = s;
 }
+setInterval(updateCountdown, 1000)
 
 updateCountdown();
 console.log("Hello world");
